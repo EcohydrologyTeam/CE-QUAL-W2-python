bf8a037193c05187984d0498583bb79fb16e0a4d
Todd Steissberg <todd.steissberg@gmail.com>
2023-06-13 00:55:57 -0700

    major updates

--- src/prototypes/CE-QUAL-W2-plot-app/plot_cequalw2_data.py
+++ src/prototypes/CE-QUAL-W2-plot-app/plot_cequalw2_data.py
@@ -105,8 +105,53 @@ class CSVPlotApp(qtw.QMainWindow):
         self.statistics_tab_layout.addWidget(self.stats_table)
         self.statistics_tab.setLayout(self.statistics_tab_layout)
 
+        # Create a new tab and a QTableWidget
+        self.data_tab = qtw.QWidget()
+        self.data_table = qtw.QTableWidget(self.data_tab)
+        self.tab_widget.addTab(self.data_tab, "Data")
+
+        # Set layout for data_tab
+        self.data_tab_layout = qtw.QVBoxLayout()
+        self.data_tab_layout.addWidget(self.data_table)
+        self.data_tab.setLayout(self.data_tab_layout)
+
+        # Fill the QTableWidget with data
+        self.update_data_table()
+
         # Set tabs as central widget
         self.setCentralWidget(self.tab_widget)
+
+
+    def update_data_table(self):
+        if self.data is not None:
+            # Convert DataFrame to numpy array for efficiency
+            array_data = self.data.values
+
+            # Get the datetime index as an array
+            # datetime_array = self.data.index.to_numpy()
+            datetime_index = self.data.index.to_series().dt.strftime('%m/%d/%Y %H:%M')
+            datetime_strings = datetime_index.tolist()
+
+            header = ['Date']
+            for col in self.data.columns:
+                header.append(col)
+
+            number_rows, number_columns = array_data.shape
+            self.data_table.setRowCount(number_rows)
+            self.data_table.setColumnCount(number_columns + 1)
+            self.data_table.setHorizontalHeaderLabels(header)
+
+            for row in range(number_rows):
+                for column in range(number_columns + 1):
+                    if column == 0:
+                        item = qtw.QTableWidgetItem(datetime_strings[row])
+                        item.setTextAlignment(0x0082)
+                    else:
+                        value = array_data[row, column - 1]
+                        value_text = f'{value:.4f}'
+                        item = qtw.QTableWidgetItem(value_text)
+                        item.setTextAlignment(0x0082)
+                    self.data_table.setItem(row, column, item)
 
 
     def parse_year_csv(self, w2_control_file_path):
@@ -143,19 +188,15 @@ class CSVPlotApp(qtw.QMainWindow):
         w2_file_type = None
 
         if glob.glob(path1):
-            print(f'{path1} was found!')
             w2_control_file_path = path1
             w2_file_type = "CSV"
         elif glob.glob(path2):
-            print(f'{path2} was found!')
             w2_control_file_path = path2
             w2_file_type = "CSV"
         elif glob.glob(path3):
-            print(f'{path3} was found!')
             w2_control_file_path = path3
             w2_file_type = "NPT"
         elif glob.glob(path4):
-            print(f'{path4} was found!')
             w2_control_file_path = path4
             w2_file_type = "NPT"
         else:
@@ -167,8 +208,6 @@ class CSVPlotApp(qtw.QMainWindow):
             self.parse_year_csv(w2_control_file_path)
         elif w2_file_type == "NPT":
             self.parse_year_npt(w2_control_file_path)
-
-        return
 
 
     def update_year(self, text):
@@ -198,7 +237,9 @@ class CSVPlotApp(qtw.QMainWindow):
             elif extension.lower() == '.csv':
                 data_columns = w2.get_data_columns_csv(self.file_path)
             else:
-                raise ValueError('Only *.csv, *.npt, and *.opt files are supported.')
+                file_dialog.close()
+                self.show_warning_dialog('Only *.csv, *.npt, and *.opt files are supported.')
+                return
 
             # Get model year
             self.get_model_year()
@@ -207,50 +248,57 @@ class CSVPlotApp(qtw.QMainWindow):
             try:
                 self.data = w2.read(self.file_path, self.year, data_columns)
             except IOError:
-                self.show_warning_dialog()
+                self.show_warning_dialog(f'An error occurred while opening {self.filename}')
                 file_dialog.close()
+
+        self.update_data_table()
+        self.make_stats_table()
+
+
+    def make_stats_table(self):
+        if self.data is None:
+            return
+
+        # Compute statistics
+        statistics = self.data.describe().reset_index()
+        self.stats_table.setRowCount(len(statistics))
+        self.stats_table.setColumnCount(len(self.data.columns) + 1)
+        # Note: col = 0 is the index column, which lists the statistics names
+        # Therefore, the total number of columns is len(data.columns) + 1
+        header = ['']
+        for col in self.data.columns:
+            header.append(col)
+        self.stats_table.setHorizontalHeaderLabels(header)
+        for row in range(len(statistics)):
+            for col in range(len(self.data.columns) + 1):
+                # See note above about the number of columns
+                value = statistics.iloc[row, col]
+                try:
+                    if col == 0:
+                        value_text = str(value)
+                    elif row == 0: 
+                        value_text = f'{int(value):d}' # format the "count" statistic as an integer
+                    else:
+                        value_text = f'{value:.2f}' # format everything else as a float
+                except ValueError:
+                    value_text = str(value)
+                item = qtw.QTableWidgetItem(value_text)
+                item.setTextAlignment(0x0082)
+                self.stats_table.setItem(row, col, item)
 
 
     def plot_data(self):
-        if self.data is not None:
-            self.figure.clear()
-            ax = self.figure.add_subplot(111)
-            # self.data.plot(ax=ax)
-            if self.PLOT_TYPE == 'plot':
-                w2.plot(self.data, fig=self.figure, ax=ax)
-            elif self.PLOT_TYPE == 'multiplot':
-                w2.multi_plot(self.data, fig=self.figure, ax=ax)
-            self.canvas.draw()
+        if self.data is None:
+            return
 
-            # Compute statistics
-            statistics = self.data.describe().reset_index()
-            print(statistics)
-            self.stats_table.setRowCount(len(statistics))
-            self.stats_table.setColumnCount(len(self.data.columns) + 1)
-            # Note: col = 0 is the index column, which lists the statistics names
-            # Therefore, the total number of columns is len(data.columns) + 1
-            header = ['']
-            for col in self.data.columns:
-                header.append(col)
-            self.stats_table.setHorizontalHeaderLabels(header)
-            for row in range(len(statistics)):
-                for col in range(len(self.data.columns) + 1):
-                    # See note above about the number of columns
-                    value = statistics.iloc[row, col]
-                    print(row, col, value)
-                    try:
-                        if col == 0:
-                            value_text = str(value)
-                        elif row == 0: 
-                            value_text = f'{int(value):d}' # format the "count" statistic as an integer
-                        else:
-                            value_text = f'{value:.2f}' # format everything else as a float
-                    except ValueError:
-                        value_text = str(value)
-                    print(row, col, value_text)
-                    item = qtw.QTableWidgetItem(value_text)
-                    item.setTextAlignment(0x0082)
-                    self.stats_table.setItem(row, col, item)
+        self.figure.clear()
+        ax = self.figure.add_subplot(111)
+        # self.data.plot(ax=ax)
+        if self.PLOT_TYPE == 'plot':
+            w2.plot(self.data, fig=self.figure, ax=ax)
+        elif self.PLOT_TYPE == 'multiplot':
+            w2.multi_plot(self.data, fig=self.figure, ax=ax)
+        self.canvas.draw()
 
 
     def plot_option_changed(self):
@@ -261,13 +309,12 @@ class CSVPlotApp(qtw.QMainWindow):
             self.PLOT_TYPE = 'multiplot'
 
 
-    def show_warning_dialog(self):
-        app = qtw.QApplication([])
+    def show_warning_dialog(self, message):
         message_box = qtw.QMessageBox()
         message_box.setIcon(qtw.QMessageBox.Critical)
         message_box.setWindowTitle('Error')
-        message_box.setText(f'An error occurred while opening {self.filename}')
-        message_box.setStandardButtons(qtw.QMessageBox.Close)
+        message_box.setText(message)
+        # message_box.setStandardButtons(qtw.QMessageBox.Close)
         message_box.exec_()
 
 
